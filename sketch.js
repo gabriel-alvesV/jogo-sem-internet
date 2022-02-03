868e20f15bfe461578bc2a5b9ddb0c85c9fb3a5b
gabriel-alvesV <98417878+gabriel-alvesV@users.noreply.github.com>
2022-02-03 11:07:18 -0300

    T-Rex

--- sketch.js
+++ sketch.js
@@ -1,35 +1,43 @@
 var Rex, RexCorrendo;
 var chao;
+var chao2;
 var grama;
-function preload(){
-  RexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
-  grama= loadImage('ground2.png');
 
+function preload(){
+RexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
+grama= loadImage('ground2.png');
 }
 
 function setup(){
-  createCanvas(600,200);
-  Rex = createSprite(50, 160, 20, 50);
-  Rex.addAnimation("correndo", RexCorrendo);
-  Rex.scale = 0.5;
-  borda = createEdgeSprites();
-  chao=createSprite(200,180,400,20);
-  chao.addImage('grama',grama);
-  chao.x=chao.width/2;
+createCanvas(600,200);
+Rex = createSprite(50, 160, 20, 50);
+Rex.addAnimation("correndo", RexCorrendo);
+Rex.scale = 0.5;
 
+borda = createEdgeSprites();
+
+chao=createSprite(200,180,400,20);
+chao.addImage('grama',grama);
+chao.x=chao.width/2;
+
+chao2=createSprite(200,190,400,10);
+chao2.visible=false;
 }
 
 function draw(){
-  background("white");
-  chao.velocityX=-2;
-   if(chao.x<0){
-     chao.x=chao.width/2;
-   }
-  if(keyDown("space")){
-    Rex.velocityY = -10;
-  }
-  Rex.velocityY += 1;
+background("white");
+console.log(Rex.y);
 
-  Rex.collide(chao);
-  drawSprites();
+chao.velocityX=-2;
+if(chao.x<0){
+chao.x=chao.width/2;
+}
+
+if(keyDown("space")&&Rex.y>=150){
+Rex.velocityY = -10;
+}
+Rex.velocityY += 1;
+Rex.collide(chao2);
+
+drawSprites();
 }
