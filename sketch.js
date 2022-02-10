4f817fa572fe6f65c34a131adf99e2aa53680d64
gabriel-alvesV <98417878+gabriel-alvesV@users.noreply.github.com>
2022-02-10 17:37:27 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ var Rex, RexCorrendo;
 var chao;
 var chao2;
 var grama;
+var nuvem;
+var nuvemImagem;
 
 function preload(){
 RexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
 grama= loadImage('ground2.png');
+nuvemImagem=loadImage('cloud.png');
 }
 
 function setup(){
@@ -22,11 +25,14 @@ chao.x=chao.width/2;
 
 chao2=createSprite(200,190,400,10);
 chao2.visible=false;
+
+var numero = Math.round(random(1, 100));
+console.log(numero);
 }
 
 function draw(){
 background("white");
-console.log(Rex.y);
+//console.log(Rex.y);
 
 chao.velocityX=-2;
 if(chao.x<0){
@@ -39,5 +45,20 @@ Rex.velocityY = -10;
 Rex.velocityY += 1;
 Rex.collide(chao2);
 
+nuvens();
+
 drawSprites();
+}
+
+function nuvens(){
+    if(frameCount%60===0){
+        nuvem=createSprite(600,100,40,10);
+        nuvem.addImage(nuvemImagem);
+        nuvem.y=Math.round(random(1,120));
+        nuvem.scale=0.6
+ nuvem.velocityX=-3;
+ nuvem.depth=Rex.depth;
+ Rex.depth+=1;
+    }
+ 
 }
